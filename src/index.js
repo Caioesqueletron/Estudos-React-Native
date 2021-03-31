bb6e3271776fca0b6dab2a24ba179744c8567818
caioesqueletron <caiocaio620@gmail.com>
2021-03-30 22:57:31 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,51 @@
-import React from 'react'
-import {View} from 'react-native'
-
-
+import React, {useEffect, useState} from 'react'
+import {SafeAreaView,FlatList,Text,StyleSheet, StatusBar} from 'react-native'
+import api from './services/api'
+//View: container div do html, asider, header,footer
+//Text: p, span, strong, h1,h2,h3
+//Não possuem valor semantico
+//Não possuem estilização propria
+//
 export default function App(){
-    return <View/>
+    const [projects, setProjects] = useState([])
+
+    useEffect(()=>{
+        api.get('projects').then(response => {
+            setProjects(response.data)
+            console.log(response.data)
+        })
+    },[])
+    return (
+    <>
+    <StatusBar barStyle="light-content" backgroundColor="black" />
+    <SafeAreaView  style={styles.container}>
+    <FlatList data={projects} keyExtractor={project => project.id} renderItem={({item:project}) => (
+        <Text  style={styles.project}>{project.title}</Text>
+    )}/>
+    </SafeAreaView>
+   
+   { /*<View style={styles.container}>
+        {projects.map(project => (
+        <Text key={project.id} style={styles.project}>{project.title}</Text>))}
+        </View>*/}
+    </>
+        )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: '#7159c1',
+       
+       
+    },
+    title: {
+        color: '#FFF',
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    project: {
+        color: '#FFF',
+        fontSize:30
+    }
+})
